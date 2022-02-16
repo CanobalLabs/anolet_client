d4e4e0b0f3224215efeab94cdea2c5f91a59f17f
Léo Badeaux <leobadeaux@gmail.com>
2022-02-16 11:59:11 -0500

    Add new avatars to backend

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http")
 const Websocket = require('ws');
 
 const wss = new Websocket.Server({ noServer: true });
-var avatars = ["./LeoBadeaux.png", "./Avatar2.png", "./Avatar3.png"];
+var avatars = ["./LeoBadeaux.png", "./Avatar2.png", "./Avatar3.png", "./Avatar4.png", "./Avatar5.png", "./Avatar6.png", "./Avatar7.png", "./Avatar8.png", "./Avatar9.png", "./Avatar10.png", "./Avatar11.png"];
 
 Array.prototype.random = function () {
   return this[Math.floor((Math.random()*this.length))];
