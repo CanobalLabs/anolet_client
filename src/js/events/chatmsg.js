ead9fad06cbb4e13495d3d2230597fa80f684afd
Léo Badeaux <79180322+LeoBadeaux@users.noreply.github.com>
2022-03-02 08:52:12 -0500

    fix bold?

--- src/js/events/chatmsg.js
+++ src/js/events/chatmsg.js
@@ -2,7 +2,7 @@ module.exports = function (msg) {
     var chat = document.createElement("div");
     var username = document.createElement("b");
     username.innerText = `${msg.username}: `;
-    chat.appendChild(username);
-    chat.innerText +=  ` ${msg.message}`;
-    document.getElementById("log").appendChild(chat);
+    var chatel = document.getElementById("log").appendChild(chat);
+    chatel.appendChild(username);
+    chatel.innerText +=  ` ${msg.message}`;
 }
