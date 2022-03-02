49d4164d2b80bc31f31f6001ffe92c5553746341
Léo Badeaux <79180322+LeoBadeaux@users.noreply.github.com>
2022-03-02 09:47:22 -0500

    fix bold 3

--- src/js/events/chatmsg.js
+++ src/js/events/chatmsg.js
@@ -1,8 +1,14 @@
+function escapeHtml(unsafe)
+{
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
 module.exports = function (msg) {
     var chat = document.createElement("div");
-    var username = document.createElement("b");
-    username.innerText = `${msg.username}: `;
-    var chatel = document.getElementById("log").appendChild(chat);
-    chatel.appendChild(username);
-    chatel.innerText +=  ` ${msg.message}`;
+    chat.innerHTML = `<b>${escapeHtml(msg.username)}: </b> ${escapeHtml(msg.message)}`;
+    document.getElementById("log").appendChild(chat);
 }
