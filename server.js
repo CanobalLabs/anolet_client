2ed405ffad71d42661b914dd8ca4839e1be3f0d8
Léo Badeaux <46432997+UntrustableRus@users.noreply.github.com>
2022-04-18 20:14:26 -0400

    fix?

--- server.js
+++ server.js
@@ -56,8 +56,8 @@ const { contextIsolated } = require("process");
 
     wss.on('connection', async (ws, req) => {
         if (!req.headers['user-agent']) return ws.close();
-        if (await client.sIsMember("addresses", req.socket.remoteAddress)) return ws.close();
-        client.sAdd("addresses", req.socket.remoteAddress);
+        if (await client.sIsMember("addresses", ws._socket.remoteAddress)) return ws.close();
+        client.sAdd("addresses", ws._socket.remoteAddress);
         ws.id = wss.getUUID();
         var randav = avatars.random();
         console.log(chalk.black.bgGreen(" Connection ") + " " + ws.id);
