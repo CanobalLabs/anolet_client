4d4d47831d42567def4f78999485d19e8336b2a0
Léo Badeaux <46432997+UntrustableRus@users.noreply.github.com>
2022-04-20 20:58:50 -0400

    argh

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const chalk = require("chalk")
 const wss = new Websocket.Server({ noServer: true });
 var avatars = ["/Avatar1.png", "/Avatar2.png", "/Avatar3.png", "/Avatar4.png", "/Avatar5.png", "/Avatar6.png", "/Avatar7.png", "/Avatar8.png", "/Avatar9.png", "/Avatar10.png", "/Avatar11.png", "/Avatar12.png"];
 const { createClient } = require("redis");
-const { contextIsolated } = require("process");
-const { setInterval } = require("timers/promises");
 
 (async () => {
 
