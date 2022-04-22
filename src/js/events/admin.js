dd02ceef2598a4489a56fb367c9d97bebf0b55be
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-04-22 09:53:38 -0400

    fix maxlength

--- src/js/events/admin.js
+++ src/js/events/admin.js
@@ -3,7 +3,7 @@ module.exports = function(msg) {
     if (document.getElementById("player-" + msg.plrid).classList.contains("me")) {
         document.iamadmin = true;
         console.log("I am admin");
-        document.getElementById("chatbox").maxLength = "";
+        document.getElementById("chatbox").maxLength = "500000";
         // Clear interval
     }
 }
