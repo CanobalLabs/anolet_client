6c8188121db71e0e59dbf6c9c2928ebc31ea837a
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2022-06-13 13:37:19 -0400

    Players fade out when leaving

--- src/js/events/exit.js
+++ src/js/events/exit.js
@@ -1,3 +1,6 @@
 module.exports = function(msg) {
-    document.getElementById("player-" + msg.plrid).remove();
+    document.getElementById("player-" + msg.plrid).style.opacity = "0";
+    setTimeout(function(){
+        document.getElementById("player-" + msg.plrid).remove();
+    }, 500);
 }
