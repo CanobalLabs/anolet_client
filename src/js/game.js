8984c2c40091d96a66c7dbc6b68c06bd981b6308
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2022-06-14 02:40:02 -0400

    Refactoring

--- src/js/game.js
+++ src/js/game.js
@@ -3,222 +3,179 @@ import "../scss/main.scss"; // Import SCSS
 import "./animation/logo" // Import animation for the logo
 import '@jamescoyle/svg-icon'
 var pointInPolygon = require('point-in-polygon');
-var polygon = [ [ 0, 79.73333333333333 ], [ 21.809895833333343, 77.86666666666667 ], [ 37.565104166666664, 76.13333333333333 ], [ 45.638020833333336, 73.06666666666666 ], [ 49.0234375, 69.46666666666667 ], [ 50, 71.33333333333333 ], [ 51.953125, 70.53333333333333 ], [ 60.15625, 69.46666666666667 ], [ 99.93489583333333, 70.66666666666667 ], [ 99.93489583333333, 0 ], [ 0, 0.13333333333332575 ], [ 0, 79.73333333333333 ] ];
-var poly2 = [
-    [
-        48.958333333333336,
-        93.86666666666667
-    ],
-    [
-        50.78125,
-        91.6
-    ],
-    [
-        51.10677083333333,
-        91.6
-    ],
-    [
-        51.43229166666667,
-        92
-    ],
-    [
-        51.5625,
-        91.46666666666667
-    ],
-    [
-        53.7109375,
-        93.73333333333333
-    ],
-    [
-        53.58072916666667,
-        94.66666666666667
-    ],
-    [
-        55.79427083333333,
-        96.93333333333334
-    ],
-    [
-        55.72916666666667,
-        99.6
-    ],
-    [
-        54.296875,
-        99.6
-    ],
-    [
-        50.71614583333333,
-        99.73333333333333
-    ],
-    [
-        50.06510416666667,
-        99.2
-    ],
-    [
-        49.609375,
-        99.46666666666667
-    ],
-    [
-        48.958333333333336,
-        99.06666666666666
-    ],
-    [
-        48.828125,
-        98.13333333333334
-    ],
-    [
-        48.828125,
-        96.66666666666667
-    ],
-    [
-        48.958333333333336,
-        93.86666666666667
-    ]
-]
 function percentage(partialValue, totalValue) {
     return (100 * partialValue) / totalValue;
 };
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-start().then(wsresp => {
-    var ws = wsresp;
+// this file is becoming unmanageable. we need to clean up the code in all the files sooner or later.
+document.gameID = 1;
+axios.get("https://staging-api-infra.anolet.com/game/1").then((res) => {
+    console.log(res);
+    document.getElementById("game-name").innerText = res.data.title;
+    document.getElementById("game-author").innerText = res.data.creator.name;
+    document.getElementById("game-image").src = res.data.iconAssetURL;
 
-    // Ran by MouseClick Event
-    function moved(event) {
-        if (pointInPolygon([percentage(event.clientX, window.innerWidth), percentage(event.clientY, window.innerHeight)], polygon)) {
-            return;
-        }
-        if (pointInPolygon([percentage(event.clientX, window.innerWidth), percentage(event.clientY, window.innerHeight)], poly2)) {
-            return;
-        }
-        ws.ws.send(JSON.stringify({
-            type: "pos",
-            x: percentage(event.clientX, window.innerWidth),
-            y: percentage(event.clientY, window.innerHeight)
-        }));
+    var styleSheet = document.createElement("style")
+    styleSheet.innerHTML = `
+    .player {
+        transition: top ${res.data.worldSettings.defaultSpeed}s ease-out, left ${res.data.worldSettings.defaultSpeed}s ease-out, opacity ${res.data.worldSettings.defaultSpeed}s ease-out;
     }
-
-    // Ran when username is chosen
-    function username(event, el) {
-        if (event.which == 13) {
-            if (event.target.value.length > 20 || event.target.value.length < 3) return;
-            ws.ws.send(JSON.stringify({
-                type: "setname",
-                username: event.target.value
-            }));
-            el.placeholder = "Send a chat message"
-            el.value = "";
-            el.maxLength = "100";
-            el.attributes.onkeydown.value = "chat(event, this)";
+    `
+    document.head.appendChild(styleSheet);
+    var defaultZone = res.data.zones.find(zone => zone.id == res.data.worldSettings.defaultZone);
+    var currentZone = defaultZone;
+    defaultZone.layers.forEach(layer => {
+        var layerElement = document.createElement("img");
+        layerElement.classList.add("layer");
+        layerElement.src = layer.assetURL;
+        if (layer.layer == 1) {
+            layerElement.style = "z-index: 1;";
+        } else {
+            layerElement.style = `z-index: ${layer.layer + 1};`
         }
-    };
-    window.username = username;
+        document.getElementById("game").appendChild(layerElement);
+    });
+    start().then(wsresp => {
+        var ws = wsresp;
 
-    // Ran when chat is sent
-    function chat(event, el) {
-        if (event.which == 13) {
-            if (document?.iamadmin != true && (event.target.value.length > 100 || event.target.value.length < 3)) return;
+        // Ran by MouseClick Event
+        function moved(event) {
+            currentZone.boundaryPolylines.forEach(boundary => {
+                if (pointInPolygon([percentage(event.clientX, window.innerWidth), percentage(event.clientY, window.innerHeight)], boundary)) {
+                    return;
+                }
+            });
             ws.ws.send(JSON.stringify({
-                type: "chat",
-                message: event.target.value
+                type: "pos",
+                x: percentage(event.clientX, window.innerWidth),
+                y: percentage(event.clientY, window.innerHeight)
             }));
-            if (!document.iamadmin) {
-                el.disabled = true;
-                var timeLeft = 3;
-                el.placeholder = "Please wait 3 seconds...";
-                var timeout = setInterval(() => {
-                    timeLeft--;
-                    el.placeholder = "Please wait " + timeLeft + " seconds...";
-                    if (timeLeft == 0) {
+        }
+
+        // Ran when username is chosen
+        function username(event, el) {
+            if (event.which == 13) {
+                if (event.target.value.length > 20 || event.target.value.length < 3) return;
+                ws.ws.send(JSON.stringify({
+                    type: "setname",
+                    username: event.target.value
+                }));
+                el.placeholder = "Send a chat message"
+                el.value = "";
+                el.maxLength = "100";
+                el.attributes.onkeydown.value = "chat(event, this)";
+            }
+        };
+        window.username = username;
+
+        // Ran when chat is sent
+        function chat(event, el) {
+            if (event.which == 13) {
+                if (document?.iamadmin != true && (event.target.value.length > 100 || event.target.value.length < 3)) return;
+                ws.ws.send(JSON.stringify({
+                    type: "chat",
+                    message: event.target.value
+                }));
+                if (!document.iamadmin) {
+                    el.disabled = true;
+                    var timeLeft = 3;
+                    el.placeholder = "Please wait 3 seconds...";
+                    var timeout = setInterval(() => {
+                        timeLeft--;
+                        el.placeholder = "Please wait " + timeLeft + " seconds...";
+                        if (timeLeft == 0) {
+                            el.placeholder = "Send a chat message";
+                            el.disabled = false
+                        }
+                    }, 1000);
+                    setTimeout(function () { clearInterval(timeout) }, 3000);
+                    setTimeout(function () {
                         el.placeholder = "Send a chat message";
                         el.disabled = false
-                    }
-                }, 1000);
-                setTimeout(function () { clearInterval(timeout) }, 3000);
-                setTimeout(function () {
-                    el.placeholder = "Send a chat message";
-                    el.disabled = false
-                }, 3000); // bandaid solution
+                    }, 3000); // bandaid solution
+                }
+                el.value = "";
             }
-            el.value = "";
+        };
+        window.chat = chat;
+
+        // Ran when character is selected
+        function avatar(id) {
+            ws.ws.send(JSON.stringify({
+                type: "setavatar"
+            }));
+        };
+        window.avatar = avatar; // Make it available to the frontend or else webpack will change the name
+
+
+        function kick(id) {
+            ws.ws.send(JSON.stringify({
+                type: "kick",
+                id: id.substring(7)
+            }));
         }
-    };
-    window.chat = chat;
+        window.kick = kick;
+        // aghhhhh this is so bad, REFACTORING NEEDED EVENTUALLY (OF THE ENTIRE CLIENT)
 
-    // Ran when character is selected
-    function avatar(id) {
-        ws.ws.send(JSON.stringify({
-            type: "setavatar"
-        }));
-    };
-    window.avatar = avatar; // Make it available to the frontend or else webpack will change the name
+        // Assign the move event to when the mouse is clicked
+        document.getElementById("game").addEventListener("click", moved);
 
-
-    function kick(id) {
-        ws.ws.send(JSON.stringify({
-            type: "kick",
-            id: id.substring(7)
-        }));
-    }
-    window.kick = kick;
-    // aghhhhh this is so bad, REFACTORING NEEDED EVENTUALLY (OF THE ENTIRE CLIENT)
-
-    // Assign the move event to when the mouse is clicked
-    document.getElementById("game").addEventListener("click", moved);
-
-    document.getElementById("chat_toggle").addEventListener("click", function () {
-        if (document.getElementById("chat").style.opacity == "0") {
-            document.getElementById("chat").style["z-index"] = "1";
-            document.getElementById("chat").style.opacity = "1";
-            document.getElementById("chat_toggle").style.filter = "invert(1)";
-        } else {
-            setTimeout(function () {
-                document.getElementById("chat").style["z-index"] = "-1";
-            }, 200);
-            document.getElementById("chat").style.opacity = "0";
-            document.getElementById("chat_toggle").style.filter = "invert(0)";
-        }
-    });
+        document.getElementById("chat_toggle").addEventListener("click", function () {
+            if (document.getElementById("chat").style.opacity == "0") {
+                document.getElementById("chat").style["z-index"] = "1";
+                document.getElementById("chat").style.opacity = "1";
+                document.getElementById("chat_toggle").style.filter = "invert(1)";
+            } else {
+                setTimeout(function () {
+                    document.getElementById("chat").style["z-index"] = "-1";
+                }, 200);
+                document.getElementById("chat").style.opacity = "0";
+                document.getElementById("chat_toggle").style.filter = "invert(0)";
+            }
+        });
 
 
-    document.getElementById("info_toggle").addEventListener("click", function () {
-        if (document.getElementById("info").style.opacity == "0") {
-            document.getElementById("info").style["z-index"] = "1";
-            document.getElementById("info").style.opacity = "1";
-            document.getElementById("info_toggle").style.filter = "invert(1)";
-        } else {
-            setTimeout(function () {
-                document.getElementById("info").style["z-index"] = "-1";
-            }, 200);
-            document.getElementById("info").style.opacity = "0";
-            document.getElementById("info_toggle").style.filter = "invert(0)";
-        }
-    });
+        document.getElementById("info_toggle").addEventListener("click", function () {
+            if (document.getElementById("info").style.opacity == "0") {
+                document.getElementById("info").style["z-index"] = "1";
+                document.getElementById("info").style.opacity = "1";
+                document.getElementById("info_toggle").style.filter = "invert(1)";
+            } else {
+                setTimeout(function () {
+                    document.getElementById("info").style["z-index"] = "-1";
+                }, 200);
+                document.getElementById("info").style.opacity = "0";
+                document.getElementById("info_toggle").style.filter = "invert(0)";
+            }
+        });
 
 
-    document.getElementById("menu_toggle").addEventListener("click", function () {
-        if (document.getElementById("menu").style.opacity == "0") {
-            document.getElementById("menu").style["z-index"] = "0";
-            document.getElementById("menu").style.opacity = "1";
-            document.getElementById("menu_toggle").style.filter = "invert(1)";
-        } else {
+        document.getElementById("menu_toggle").addEventListener("click", function () {
+            if (document.getElementById("menu").style.opacity == "0") {
+                document.getElementById("menu").style["z-index"] = "0";
+                document.getElementById("menu").style.opacity = "1";
+                document.getElementById("menu_toggle").style.filter = "invert(1)";
+            } else {
+                setTimeout(function () {
+                    document.getElementById("menu").style["z-index"] = "-1";
+                }, 200);
+                document.getElementById("menu").style.opacity = "0";
+                document.getElementById("menu_toggle").style.filter = "invert(0)";
+            }
+        });
+        document.getElementById("menu_close").addEventListener("click", function () {
             setTimeout(function () {
                 document.getElementById("menu").style["z-index"] = "-1";
             }, 200);
             document.getElementById("menu").style.opacity = "0";
             document.getElementById("menu_toggle").style.filter = "invert(0)";
-        }
-    });
-    document.getElementById("menu_close").addEventListener("click", function () {
-        setTimeout(function () {
-            document.getElementById("menu").style["z-index"] = "-1";
-        }, 200);
-        document.getElementById("menu").style.opacity = "0";
-        document.getElementById("menu_toggle").style.filter = "invert(0)";
-    });
-    document.getElementById("avatar").addEventListener("click", function () {
-        avatar();
-    });
-    console.log(ws)
+        });
+        document.getElementById("avatar").addEventListener("click", function () {
+            avatar();
+        });
+        console.log(ws)
 
+    });
 });
