624c9f26b36e3a6a9694d4bcc976d005a9424201
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2022-08-23 21:44:11 -0400

    Patch FS errors

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = (env, argv) => {
                 "path": require.resolve("path-browserify"),
                 "buffer": require.resolve("buffer/"),
                 "stream": require.resolve("stream-browserify"),
+                "fs": false
             }
         },
         module: {
