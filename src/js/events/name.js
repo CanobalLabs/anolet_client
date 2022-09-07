725abc4fb195895cfd0a4e44c66f97aab6a97ca5
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-09-07 10:29:46 -0400

    Update name.js

--- src/js/events/name.js
+++ src/js/events/name.js
@@ -1,7 +1,7 @@
 module.exports = function (msg) {
     document.getElementById("player-" + msg.plrid).children[1].innerText = msg.username;
     if (document.iamadmin) {
-        document.getElementById("player-" + msg.plrid).children[1].innerHTML += `<span id="kick-${msg.plrid}" style="margin-left: 5px; cursor: pointer;" class="mdi mdi-close-circle"></span>`;
+        document.getElementById("player-" + msg.plrid).children[1].innerHTML += `<div id="kick-${msg.plrid}"><span style="margin-left: 5px; cursor: pointer; z-index: 5000;" class="mdi mdi-close-circle"></span></div>`;
         document.getElementById("kick-" + msg.plrid).addEventListener("click", function () {
             kick("player-" + msg.plrid);
         });
