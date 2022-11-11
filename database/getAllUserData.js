dedcf5bcf628a2505c3c0471fe868d34d9409dc4
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2022-11-10 20:15:44 -0500

    Move up username, format

--- database/getAllUserData.js
+++ database/getAllUserData.js
@@ -2,5 +2,8 @@ const client = require("./init");
 
 module.exports = async function (gameid) {
     // zuckerberg's favorite function
-    return await Promise.all((await client.sMembers('players:' + gameid)).map(async (key) => await client.hGetAll('player:' + gameid + ":" + key)))
+    return Promise.all(
+        (await client.sMembers('players:' + gameid)).map(async (key) =>
+            await client.hGetAll('player:' + gameid + ":" + key)
+        ))
 }
