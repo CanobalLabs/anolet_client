c52ae83490e4f720057c202ca9dbe3268a2acffa
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-12-15 08:44:43 -0500

    fix setInterval

--- api/setInterval/setInterval.js
+++ api/setInterval/setInterval.js
@@ -1,10 +1,17 @@
 module.exports = function(vm) {
     try {
-        return vm.newFunction("setInterval", (...args) => {
-            const nativeArgs = args.map(vm.dump);
-            console.log(nativeArgs)
-            return vm.newNumber(setInterval(...nativeArgs));
-        });
+        return vm.newFunction("setTimeout", (vmFnHandle, timeoutHandle) => {
+            // Make a copy because otherwise vmFnHandle does not live long enough to call after the timeout
+            const vmFnHandleCopy = vmFnHandle.dup()
+            const timeout = vm.dump(timeoutHandle)
+            const timeoutID = setTimeout(() => {
+            // callFunction(vmFnHandleCopy) will call the vm function
+            // in the context of the vm
+            // we pass vm.undefined because we need to pass something for the "this" argument
+                vm.callFunction(vmFnHandleCopy, vm.undefined)
+            }, timeout)
+            return vm.newNumber(timeoutID);
+       });
     } catch (e) {
         console.error(e);
     }
