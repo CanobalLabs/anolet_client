9ff99dc0c8cc308457d0ff86b0ec09764b3868ec
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2023-03-16 01:03:35 -0400

    fix user exiting

--- src/js/events/exit.js
+++ src/js/events/exit.js
@@ -1,9 +1,7 @@
 module.exports = function (msg) {
     if (msg.newZone == "") document.getElementById("listusr-" + msg.plrid).remove();
-    if (msg.oldZone == document.currentZone.id) {
         document.getElementById("player-" + msg.plrid).style.opacity = "0";
         setTimeout(function () {
             document.getElementById("player-" + msg.plrid).remove();
         }, 500);
-    }
 }
